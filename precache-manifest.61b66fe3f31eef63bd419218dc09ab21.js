acbca0a95713781ef5a90baff70d97e1fc400ddf
Eric <sarah@Ebio-MacBook-Air.local>
2020-09-19 08:07:36 -0600

    Updates

---
+++ precache-manifest.61b66fe3f31eef63bd419218dc09ab21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb293d57e62f9551dd0d96b7a13b741",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "3eb61d0c8997142e3623",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd5758a2e9150098a073",
+    "url": "/facerecognitionbrain/static/css/main.5857d794.chunk.css"
+  },
+  {
+    "revision": "3eb61d0c8997142e3623",
+    "url": "/facerecognitionbrain/static/js/2.7ff08d9a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.7ff08d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5758a2e9150098a073",
+    "url": "/facerecognitionbrain/static/js/main.3136069e.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
