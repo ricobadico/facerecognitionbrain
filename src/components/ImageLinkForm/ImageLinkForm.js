41c00749cc260230f121c7df3e1c97c7f655497e
ricobadico <biondiceric@gmail.com>
2021-02-03 21:32:30 -0700

    added some text to clarify text input

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ import './ImageLinkForm.css';
      return (
          <div>
              <p className='f3'>
-                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+                 {'This Magic Brain will detect faces in an image url. Paste one here and give it a try!'}
              </p>
              <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
